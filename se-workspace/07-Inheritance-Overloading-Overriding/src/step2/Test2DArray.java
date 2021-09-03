af3105959921ceefa0036e43b63e2681ab200bb7
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-09-04 01:14:25 +0900

    Day-07 수업 내용

---
+++ se-workspace/07-Inheritance-Overloading-Overriding/src/step2/Test2DArray.java
@@ -0,0 +1,36 @@
+package step2;
+
+public class Test2DArray {
+	public static void main(String[] args) {
+		// 다차원 배열
+		// 3년간 매월 강우량 저장 -> 2차원 배열
+		int rain[][] = new int[3][12];
+		// 첫해 첫월에 강우량 저장
+		rain[0][0] = 100;
+		// 첫해 2월
+		rain[0][1] = 120;
+		// 마지막해 12월
+		rain[2][11] = 200;
+		
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<12; j++) {
+				System.out.print(rain[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("*************************");
+		
+		// 오리중학교 1학년 3개반 2학년 5개반 3학년 6개반
+		int school[][] = new int[3][];
+		school[0] = new int[3];
+		school[1] = new int[5];
+		school[2] = new int[6];
+		
+		// 오리중학교 마지막 학년의 마지막 반에 20명 할당 후 출력
+		int i = school.length - 1; // 학년의 마지막 인덱스 2
+		int j = school[i].length - 1; // 학년에 배정된 반의 마지막 인덱스 5
+		school[i][j] = 20;
+		System.out.println(school[i][j]);
+	}
+}
---
+++ se-workspace/07-Inheritance-Overloading-Overriding/src/step3/TestOverloading2.java
@@ -0,0 +1,48 @@
+package step3;
+
+class Human {
+	private String name;
+	private int salary;
+	
+	Human() {}
+	
+	// 생성자 오버로딩: 동일한 이름으로 다양한 매개변수를 정의
+	Human(String name) {
+		this.name = name;
+	}
+	
+	// 생성자 오버로딩
+	Human(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+}
+
+public class TestOverloading2 {
+	public static void main(String[] args) {
+		Human h1 = new Human();
+		System.out.println(h1.getName() + " " + h1.getSalary()); // null 0
+		Human h2 = new Human("김연경"); // 다양한 인자값으로 객체를 생성
+		System.out.println(h2.getName() + " " + h2.getSalary()); // 김연경 0
+		Human h3 = new Human("강백호", 300); // 생성자와 오버로딩이 되어 있으므로 다양한 인자값으로 객체를 생성할 수 있다.
+		System.out.println(h3.getName() + " " + h3.getSalary());
+	}
+}
