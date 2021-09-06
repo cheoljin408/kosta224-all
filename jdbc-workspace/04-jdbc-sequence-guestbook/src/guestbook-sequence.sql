a3de250cc677981d872beaf02904d0779fd6b1e1
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-09-06 20:43:31 +0900

    Like 연습

--- jdbc-workspace/04-jdbc-sequence-guestbook/src/guestbook-sequence.sql
+++ jdbc-workspace/04-jdbc-sequence-guestbook/src/guestbook-sequence.sql
@@ -11,3 +11,12 @@ select * from guestbook;
 insert into guestbook(guestbook_no, title, content) values(guestbook_seq.nextval, 'asd', 'zxc');
 
 commit
+
+-- like 연산자를 이용해 title에 즐 문자열이 포함된 방명록 정보를 조회
+select * from guestbook where title like '%즐%';
+
+-- 위 like 적용 sql을 PreparedStatement에 적용하는 방법은 아래와 같다
+/*
+ * '%즐%' -> '%' || ? || '%'
+ * String sql = "select * from guestbook where title like '%즐%'";
+ */
--- jdbc-workspace/04-jdbc-sequence-guestbook/src/model/GuestBookDAO.java
+++ jdbc-workspace/04-jdbc-sequence-guestbook/src/model/GuestBookDAO.java
@@ -70,4 +70,61 @@ public class GuestBookDAO {
 		}
 		return result;
 	}
+
+	/*
+	 	글 등록시에 db에서 발급한 시퀀스 정보를 dto의 guestbookNo에 할당시키도록
+	 	기존의 register메소드를 업데이트한 메소드
+	 	Connection
+	 	PreparedStatement
+	 	insert sql 실행
+	 	pstmt.close()
+	 	PreparedStatement
+	 	select sql 실행
+	 	ResultSet
+	 	closeAll()
+	 */
+	public void registerVer2(GuestBookDTO dto) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = DriverManager.getConnection(url, username, password);
+			String insertSql = "insert into guestbook(guestbook_no, title, content) values(guestbook_seq.nextval, ?, ?)";
+			pstmt = con.prepareStatement(insertSql);
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getContent());
+			pstmt.executeUpdate();
+			pstmt.close();
+			String selectSql = "select guestbook_seq.currval from dual";
+			pstmt = con.prepareStatement(selectSql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				int guestBookNo = rs.getInt(1); // insert 시점에 발급받은 시퀀스(guestbook_no에 저장된 값)의 현재값을 받아온다
+				dto.setGuestbookNo(guestBookNo); // dto에 할당한다
+			}
+		} finally {
+			closeAll(rs, pstmt, con);
+		}
+	}
+
+	public ArrayList<GuestBookDTO> getGuestBookListLikeKeyword(String keyword) throws SQLException {
+		ArrayList<GuestBookDTO> list = new ArrayList<GuestBookDTO>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = DriverManager.getConnection(url, username, password);
+			String sql = "select guestbook_no, title, content from guestbook where title like '%' || ? || '%'";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, keyword);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				list.add(new GuestBookDTO(rs.getInt(1), rs.getString(2), rs.getString(3)));
+			}
+		} finally {
+			closeAll(rs, pstmt, con);
+		}
+		return list;
+	}
 }
