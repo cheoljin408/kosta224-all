ecbddc51b989094d0a94312580c06b5f8a44ea05
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-09-08 22:05:57 +0900

    Bank Project Step1,2 수정

--- jdbc-workspace/07-jdbc-bank-project/src/test/TestUnit1.java
+++ jdbc-workspace/07-jdbc-bank-project/src/test/TestUnit1.java
@@ -15,17 +15,17 @@ public class TestUnit1 {
 			AccountDAO dao = new AccountDAO();
 			// 계좌개설
 			// 1. 정상흐름 테스트
-			/*
-			AccountVO vo = new AccountVO("아이유", "1234", 1000);
+			
+			AccountVO vo = new AccountVO("이강인", "1", 2000);
 			dao.createAccount(vo);
 			System.out.println("계좌 개설");
-			*/
-			// 2. 대안흐름 테스트
 			
+			// 2. 대안흐름 테스트
+			/*
 			AccountVO vo = new AccountVO("박보검", "1111", 999);
 			dao.createAccount(vo);
 			System.out.println("계좌 개설");
-			
+			*/
 		} catch(CreateAccountException ce) {
 			System.out.println(ce.getMessage()); // 계좌 개설기 초기 납입금은 1000원 이상이어야 합니다
 		} catch (ClassNotFoundException e) {
