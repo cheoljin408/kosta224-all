7d9bc7babfff926c2373ba6efc2b0df2fb7a2d1d
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-09-08 22:08:28 +0900

    Exception 클래스 추가

---
+++ jdbc-workspace/07-jdbc-bank-project/src/model/AccountNotFoundException.java
@@ -0,0 +1,13 @@
+package model;
+
+public class AccountNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3681249781517219295L;
+	
+	public AccountNotFoundException(String message) {
+		super(message);
+	}
+}
