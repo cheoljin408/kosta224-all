ad432211b76dee2d2cff4ab02a4048308ebdda38
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-09-15 23:12:08 +0900

    js 파일 따로 관리하기

---
+++ web-workspace/webstudy04-javascript/src/main/webapp/js/step3.js
@@ -0,0 +1,16 @@
+/*
+	step3-function.html 에서 사용하는 함수를 정의
+*/
+
+function hello() {
+	console.log("hello 함수 실행");
+	alert("hello 함수 실행");
+}
+
+function bye(message) {
+	alert(message);
+}
+
+function testParam(info, num) {
+	alert(info + " " + num);
+}
