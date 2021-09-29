86fb8801b5a34f97c50353d12f1259c75c5bee84
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-09-29 23:39:47 +0900

    Model - FoodVO

---
+++ web-workspace/webstudy11-jsp-basic/src/main/java/model/FoodVO.java
@@ -0,0 +1,44 @@
+package model;
+
+public class FoodVO {
+	private String name;
+	private String maker;
+	private int price;
+	
+	public FoodVO() {
+		super();
+	}
+
+	public FoodVO(String name, String maker, int price) {
+		super();
+		this.name = name;
+		this.maker = maker;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+}
