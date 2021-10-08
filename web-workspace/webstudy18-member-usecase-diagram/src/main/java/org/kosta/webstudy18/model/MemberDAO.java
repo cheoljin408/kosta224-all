3d34d0f1952f8ed9b4e6ae991b93cc02dcd303b4
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-10-08 23:07:19 +0900

    메소드 추가

--- web-workspace/webstudy18-member-usecase-diagram/src/main/java/org/kosta/webstudy18/model/MemberDAO.java
+++ web-workspace/webstudy18-member-usecase-diagram/src/main/java/org/kosta/webstudy18/model/MemberDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /*
  	Singleton Design Pattern을 적용: 시스템 상에서 단 한번 객체를 생성해 공유하여 사용하고자 할때 적용하는 설계 패턴
@@ -89,5 +90,87 @@ public class MemberDAO {
 		}
 		
 		return vo;
+	}
+	
+	public ArrayList<MemberVO> findMemberListByAddress(String memberAddress) throws SQLException {
+		ArrayList<MemberVO> list = new ArrayList<MemberVO>();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = getConnection();
+			String sql = "select id, name from member where address=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberAddress);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				list.add(new MemberVO(rs.getString(1), null, rs.getString(2), memberAddress));
+			}
+		} finally {
+			closeAll(rs, pstmt, con);
+		}
+		
+		return list;
+	}
+
+	public void updateMember(MemberVO memberVO) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = getConnection();
+			String sql = "update member set password=?, name=?, address=? where id=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberVO.getPassword());
+			pstmt.setString(2, memberVO.getName());
+			pstmt.setString(3, memberVO.getAddress());
+			pstmt.setString(4, memberVO.getId());
+			pstmt.executeUpdate();
+		} finally {
+			closeAll(pstmt, con);
+		}
+	}
+	
+	public void register(MemberVO vo) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = getConnection();
+			String sql = "insert into member(id, password, name, address) values(?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getName());
+			pstmt.setString(4, vo.getAddress());
+			pstmt.executeUpdate();
+		} finally {
+			closeAll(pstmt, con);
+		}
+	}
+
+	public int idCheck(String memberId) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int result = 0;
+		
+		try {
+			con = getConnection();
+			String sql = "select count(*) from member where id=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+		} finally {
+			closeAll(rs, pstmt, con);
+		}
+		
+		return result;
 	}
 }
