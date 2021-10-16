74592af51182ec56a4f33753091b23aa99e48f10
cheoljin408 <kcj409@naver.com>
2021-10-16 14:41:22 +0900

    Delete 기능 추가

--- web-workspace/webstudy20-front-controller-pattern-step5/src/main/java/org/kosta/webstudy20/controller/HandlerMapping.java
+++ web-workspace/webstudy20-front-controller-pattern-step5/src/main/java/org/kosta/webstudy20/controller/HandlerMapping.java
@@ -13,19 +13,41 @@ public class HandlerMapping {
 		return instance;
 	}
 	
-	public Controller create(String command) {
-		Controller controller = null;
+	/*
+	 	step5 version create method를 개선
+	 	실행시에 클라이언트가 전달한 command를 이용해 동적으로 객체를 생성
+	 	컨트롤러 구현 객체가 추가될 때 코드를 변경하지 않아도 된다는 장점
+	 */
+	public Controller create(String command) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		// 현재 패키지 정보를 받아온다
+		// String packageName = this.getClass().getPackage().getName();
+		// System.out.println(packageName + "." + command);
+		// client가 전달한 command가 실제 컨트롤러 클래스 명이므로 패키지 정보에 추가한다
+		// String classInfo = packageName + "." + command;
 		
-		if(command.equals("findCustomerById")) {
-			controller = new FindCustomerByIdController();
-		}
-		else if(command.equals("registerCustomer")) {
-			controller = new RegisterCustomerController();
-		}
-		else if(command.equals("updateCustomer")) {
-			controller = new UpdateCustomerController();
-		}
+		String classInfo = new StringBuilder(this.getClass().getPackage().getName()).append("." + command).toString();
 		
+		// reflection API를 이용해 동적으로 객체를 생성
+		Controller controller = (Controller) Class.forName(classInfo).newInstance();
+		System.out.println(controller);
 		return controller;
 	}
+	
+	
+	// step4 version create 코드
+//	public Controller create(String command) {
+//		Controller controller = null;
+//		
+//		if(command.equals("findCustomerById")) {
+//			controller = new FindCustomerByIdController();
+//		}
+//		else if(command.equals("registerCustomer")) {
+//			controller = new RegisterCustomerController();
+//		}
+//		else if(command.equals("updateCustomer")) {
+//			controller = new UpdateCustomerController();
+//		}
+//		
+//		return controller;
+//	}
 }
