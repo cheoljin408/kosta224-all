34ca2a37971ede50ecf1eb751a4a5473a222f20e
cheoljin408 <kcj409@naver.com>
2021-11-22 21:18:55 +0900

    Spring AOP4

---
+++ framework-workspace1/2-springAOP4-Around-TimeCheck/src/org/kosta/aop/PerformanceReportService.java
@@ -0,0 +1,39 @@
+package org.kosta.aop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
+
+/*
+ 	Cross Cutting Concern(시스템의 여러 메소드의 실행 소요 시간을 체크)을 정의한 클래스
+ */
+public class PerformanceReportService {
+	
+	private Logger log = LogManager.getLogger(getClass());
+	
+	public Object timeLogging(ProceedingJoinPoint point) throws Throwable {
+		StopWatch watch = new StopWatch();
+		watch.start();
+		Object retValue = null;
+		
+		try {
+			retValue = point.proceed(); // 실제 Core를 실행
+		} finally {
+			watch.stop();
+			long time = watch.getTotalTimeMillis();
+			
+			if(time >= 500 && time < 1000) {
+				String className = point.getTarget().getClass().getName();
+				String methodName = point.getSignature().getName();
+				log.warn(className + " " + methodName + " " + time);
+			} else if(time >= 1000) {
+				String className = point.getTarget().getClass().getName();
+				String methodName = point.getSignature().getName();
+				log.error(className + " " + methodName + " " + time);
+			}
+		}
+		
+		return retValue;
+	}
+}
